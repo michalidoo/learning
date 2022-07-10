fc234f7c25fcf663513684cb286a904bc1887f51
Neriya Rosenthal <94580999+Narnillian@users.noreply.github.com>
2022-07-10 21:45:00 +0000

    Learned about printf, changing variables, sort of branching and started loops

--- learning.c
+++ learning.c
@@ -6,7 +6,34 @@ int main() {
   char firstchar = 'M';
   
   printf("first output\n");
-  printf("")
+  printf("Number: %d\n", firstint);
+  printf("Letter: %c\n", firstchar);
+
+  
+  firstchar = 'N';
+  printf("Letter: %c\n", firstchar);
+  firstint = 194;
+  firstint = 55-33;
+  firstint = firstint+9;
+  firstint += 3;
+  firstint++;
+  firstint--;
+  ++firstint;
+
+  
+  if (firstchar == 'M') {
+    printf("M is the best!\n");
+  } else if (firstchar == 'N') {
+    printf("N is the second best!\n");
+  } else {
+    printf("The variable is not M or N :(\n");
+  }
+
+  int index = 0;
+  while (index < 10) {
+    printf("index = %d", index);
+    index = index + 1
+  }
   
   return 0;
 }
